006a7408ee2eb27ab234f30d2d85d446a8eb9eb6
RodrigoSpano <rorrospano17@gmail.com>
2023-07-26 11:09:13 -0300

    refactor

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 8000
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  app.listen(8080, () => {
+  app.listen(port, () => {
     console.log('%s listening at 8080'); // eslint-disable-line no-console
   });
 });
